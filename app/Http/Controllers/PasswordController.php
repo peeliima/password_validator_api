72bc5f0a7f08bac993c07bcbf6b8fcc5e4b1893a
Pedro Liima <pedro.henrique@pede.ai>
2023-01-09 23:16:58 -0300

    Implementa as funcoes minLowercase, minDigit, minSpecialChars e noRepeted

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -26,9 +26,16 @@ class PasswordController extends Controller
         $validate = true;
         $errors = [];
 
+        if (empty($request->rules[0])) {
+            return response ([
+                'verify' => $validate,
+                'noMatch' => $errors
+            ], $http_code);
+        }
+
         foreach ($request->rules as $rule) {
-            $method_name = $rule['rule'];
-            $value = $rule['value'];
+            $method_name = $rule['rule'] ?? '';
+            $value = $rule['value'] ?? '';
 
             $response = $this->$method_name($request->password, $value);
 
@@ -74,6 +81,72 @@ class PasswordController extends Controller
         }
 
         if ($count < $value) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function minLowercase($password, $value)
+    {
+        $count = 0;
+
+        $password_lenght = mb_strlen($password);
+
+        for ($i=0; $i < $password_lenght; $i++) {
+            $explode_password = substr($password, $i, 1);
+
+            $validate = ctype_lower($explode_password);
+
+            if ($validate == true) {
+                $count++;
+            }
+        }
+
+        if ($count < $value) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function minDigit($password, $value)
+    {
+        $password_lenght = (int) filter_var($password, FILTER_SANITIZE_NUMBER_INT);
+
+        if ($password_lenght < $value) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function minSpecialChars($password, $value)
+    {
+        $regex = "/[!@#$%^&*()-\/+{}\[\]]/";
+        $matches = [];
+
+        preg_match_all($regex, $password, $matches);
+
+        $count = count($matches[0]);
+
+        if ($count < $value) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function noRepeted($password, $value)
+    {
+        $regex = '/([a-zA-Z0-9])\1+/';
+        $matches = [];
+
+        preg_match_all($regex, $password, $matches);
+
+        $count = count($matches[0]);
+
+        if ($count > 0) {
             return false;
         }
 
